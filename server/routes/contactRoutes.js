82c309cf7c2f5955b985c3657e8419b34521a4a7
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-08-13 17:05:08 +0530

    deleted

--- server/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const protect = require("../middleware/authMiddleware");
-const { contactUs } = require("../controllers/contactController");
-
-router.post("/", protect, contactUs);
-
-module.exports = router;
