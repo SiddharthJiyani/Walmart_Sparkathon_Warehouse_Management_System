696e27fe391d257d8eef85724f9970e68fcbca44
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-08-14 22:09:43 +0530

    landing page changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,59 +4,63 @@ import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
 import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
+import walmart from "./walmart.svg";
+import factory from "./factory.svg";
+// import inventory_1 from "./bi.png";
+import inventory_2 from "./inventory_2.svg";
+import inventory_1 from "./inventory_1.svg";
 
 const Home = () => {
   return (
     <div className="home">
-      <nav className="container --flex-between ">
-        <div className="logo">
-          <RiProductHuntLine size={35} />
-        </div>
+      <nav className="--flex-between navbar">
+        <a href="/" className="logo">
+          <img src={walmart} height={100} width={200} alt="Inventory" />
+        </a>
 
         <ul className="home-links">
           <ShowOnLogout>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
+            <Link to="/register">
+              <button className=" btn ">
+                  Register
+              </button>
+            </Link>
           </ShowOnLogout>
           <ShowOnLogout>
             <li>
-              <button className="--btn --btn-primary">
-                <Link to="/login">Login</Link>
-              </button>
+            <Link to="/login">
+                <button className=" btn ">
+                  Login
+                </button>
+                </Link>
             </li>
           </ShowOnLogout>
           <ShowOnLogin>
             <li>
-              <button className="--btn --btn-primary">
-                <Link to="/dashboard">Dashboard</Link>
-              </button>
+              <Link to="/dashboard">
+                <button className="btn ">
+                  Dashboard
+                </button>
+              </Link>
             </li>
           </ShowOnLogin>
         </ul>
       </nav>
       {/* HERO SECTION */}
-      <section className="container hero">
+      <section className="hero">
         <div className="hero-text">
-          <h2>Inventory {"&"} Stock Management Solution</h2>
+          <h1>Walmart's Inventory Management System</h1>
           <p>
             Inventory system to control and manage proucts in the warehouse in
-            real timeand integrated to make it easier to develop your business.
+            real time and integrated to make it easier to develop your business.
           </p>
           <div className="hero-buttons">
-            <button className="--btn --btn-secondary">
-              <Link to="/dashboard">Free Trial 1 Month</Link>
-            </button>
-          </div>
-          <div className="--flex-start">
-            <NumberText num="14K" text="Brand Owners" />
-            <NumberText num="23K" text="Active Users" />
-            <NumberText num="500+" text="Partners" />
           </div>
         </div>
 
         <div className="hero-image">
-          <img src={heroImg} alt="Inventory" />
+          {/* <img height={480}src={inventory_2} alt="Inventory" /> */}
+          <img height={480}src={inventory_1} alt="Inventory" />
         </div>
       </section>
     </div>
